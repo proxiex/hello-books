5b9270b393a52b0c87fbe0a7dc0044bee8ce1dfa
samuel <difrentecho@gmail.com>
2017-08-29 22:10:57 +0100

    made changes to config.js file

--- src/config/config.js
+++ src/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
   test: {
     username: 'postgres',
     password: '',
-    database: 'travis',
+    database: 'test',
     host: '127.0.0.1',
     dialect: 'postgres',
     logging: false
